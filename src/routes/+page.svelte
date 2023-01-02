877bad6579f1bd31aabc54b950762c626126b732
Ali Alaa <hello@alialaa.com>
2023-01-02 11:11:46 +0200

    Code for sass-setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+	h1 {
+		color: red;
+		font-size: functions.toRem(36);
+		@include breakpoint.up('md') {
+			color: green;
+		}
+		@include breakpoint.up('lg') {
+			color: blue;
+		}
+	}
+</style>
