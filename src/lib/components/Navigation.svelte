7b0eb814ef68ed3b6abf51ba321930729d684de5
Ali Alaa <hello@alialaa.com>
2023-01-08 11:40:58 +0200

    Code for menu-styling

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -28,7 +28,7 @@
 <div class="nav-content" class:desktop class:mobile={!desktop}>
 	<nav aria-label="Main">
 		<div class="nav-content-inner">
-			<img src={logo} class="logo" alt="Spotify" width="100px" />
+			<img src={logo} class="logo" alt="Spotify" />
 			<ul>
 				{#each menuItems as item}
 					<li class:active={item.path === $page.url.pathname}>
@@ -49,3 +49,60 @@
 		</div>
 	</nav>
 </div>
+
+<style lang="scss">
+	.nav-content {
+		.logo {
+			max-width: 100%;
+			width: 130px;
+		}
+		.nav-content-inner {
+			padding: 20px;
+			min-width: var(--sidebar-width);
+			background-color: var(--sidebar-color);
+			height: 100vh;
+			overflow: auto;
+			display: none;
+			ul {
+				padding: 0;
+				margin: 20px 0 0;
+				list-style: none;
+				li {
+					&.active {
+						a {
+							opacity: 1;
+						}
+					}
+					a {
+						display: flex;
+						align-items: center;
+						text-decoration: none;
+						color: var(--text-color);
+						font-size: functions.toRem(14);
+						font-weight: 500;
+						padding: 5px;
+						margin: 10px 0;
+						opacity: 0.7;
+						transition: opacity 0.2s;
+						&:hover,
+						&:focus {
+							opacity: 1;
+						}
+						:global(svg) {
+							margin-right: 12px;
+						}
+					}
+				}
+			}
+		}
+		&.desktop {
+			position: sticky;
+			top: 0;
+			.nav-content-inner {
+				@include breakpoint.up('md') {
+					display: block;
+				}
+			}
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <input />
+<div style="height: 1000px" />
 <select>
 	<option>Option</option>
 </select>
