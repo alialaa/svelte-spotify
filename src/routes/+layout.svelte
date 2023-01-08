6ee6c7d69472711032d252a881076bec77e835a4
Ali Alaa <hello@alialaa.com>
2023-01-08 12:28:10 +0200

    Code for header-styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,20 @@
 	import '../styles/main.scss';
 	import type { LayoutData } from './$types';
 
+	let topbar: HTMLElement;
+	let scrollY: number;
+	let headerOpacity = 0;
+
+	$: if (topbar) {
+		headerOpacity = scrollY / topbar.offsetHeight < 1 ? scrollY / topbar.offsetHeight : 1;
+	}
+
 	export let data: LayoutData;
 
 	$: user = data.user;
 </script>
+
+<svelte:window bind:scrollY />
 
 <div id="main">
 	{#if user}
@@ -16,7 +26,15 @@
 		</div>
 	{/if}
 	<div id="content">
-		<main id="main-content">
+		<div id="topbar" bind:this={topbar}>
+			<div
+				class="topbar-bg"
+				style:background-color="var(--header-color)"
+				style:opacity={headerOpacity}
+			/>
+			topbar
+		</div>
+		<main id="main-content" class:logged-in={user}>
 			<slot />
 		</main>
 	</div>
@@ -27,10 +45,34 @@
 		display: flex;
 		#content {
 			flex: 1;
+			#topbar {
+				position: fixed;
+				height: var(--header-height);
+				padding: 0 15px;
+				display: flex;
+				align-items: center;
+				width: 100%;
+				z-index: 100;
+				.topbar-bg {
+					position: absolute;
+					width: 100%;
+					height: 100%;
+					top: 0;
+					left: 0;
+					z-index: -1;
+				}
+				@include breakpoint.up('md') {
+					padding: 0 30px;
+					width: calc(100% - var(--sidebar-width));
+				}
+			}
 			main#main-content {
 				padding: 30px 15px 60px;
 				@include breakpoint.up('md') {
 					padding: 30px 30px 60px;
+				}
+				&.logged-in {
+					padding-top: calc(30px + var(--header-height));
 				}
 			}
 		}
