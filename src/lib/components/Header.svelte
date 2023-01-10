ce881ba6719054d71bb1bb2d615673baddb2374f
Ali Alaa <hello@alialaa.com>
2023-01-10 11:02:53 +0200

    Code for profile-menu-button

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,10 @@
-<script>
+<script lang="ts">
 	import { browser } from '$app/environment';
 	import { Navigation } from '$components';
+	import { page } from '$app/stores';
+	import { ChevronDown } from 'lucide-svelte';
+
+	$: user = $page.data.user;
 </script>
 
 <div class="content">
@@ -9,7 +13,17 @@
 			<Navigation desktop={false} />
 		{/if}
 	</div>
-	<div class="right">Right</div>
+	<div class="right">
+		<div id="profile-button">
+			<button class="profile-button">
+				{#if user?.images && user.images.length > 0}
+					<img src={user.images[0].url} alt="" />
+				{/if}
+				{user?.display_name} <span class="visually-hidden">Profile menu</span>
+				<ChevronDown class="profile-arrow" size={22} />
+			</button>
+		</div>
+	</div>
 </div>
 
 <style lang="scss">
@@ -18,5 +32,27 @@
 		justify-content: space-between;
 		align-items: center;
 		width: 100%;
+	}
+	.profile-button {
+		background: none;
+		border: 1px solid var(--border);
+		padding: 5px;
+		border-radius: 25px;
+		display: flex;
+		align-items: center;
+		color: var(--text-color);
+		cursor: pointer;
+		:global(.profile-arrow) {
+			margin-left: 3px;
+		}
+		img {
+			width: 28px;
+			height: 28px;
+			border-radius: 100%;
+			margin-right: 10px;
+		}
+		&:hover {
+			background-color: var(--accent-color);
+		}
 	}
 </style>
