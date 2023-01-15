3ab50ef6be28d14134302083437d94defbcf516c
Ali Alaa <hello@alialaa.com>
2023-01-15 17:37:22 +0200

    Code for card-component-2

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -16,6 +16,13 @@
 			<Music aria-hidden="true" focusable="false" color="var(--light-gray)" />
 		</div>
 	{/if}
+	<h4 class="truncate-1"><a href="/{item.type}/{item.id}" title={item.name}>{item.name}</a></h4>
+	{#if item.type === 'album'}
+		<p class="truncate-1">{item.artists.map((item) => item.name).join(', ')}</p>
+	{/if}
+	{#if item.type === 'playlist'}
+		<p class="truncate-2">{item.description}</p>
+	{/if}
 </div>
 
 <style lang="scss">
@@ -25,11 +32,43 @@
 		box-shadow: 0 0 40px rgba(0, 0, 0, 0.4);
 		border-radius: 4px;
 		transition: background 0.3s;
+		position: relative;
 		&:hover {
 			background-color: var(--medium-gray);
 			.cover-placeholder {
 				background-color: var(--dark-gray);
 			}
+		}
+		h4 {
+			margin: 0 0 10px;
+			font-size: functions.toRem(16);
+			font-weight: 600;
+			line-height: 1;
+			a {
+				text-decoration: none;
+				color: var(--text-color);
+				&:focus-visible {
+					outline: none;
+					&:after {
+						outline: 2px solid var(--accent-color);
+					}
+				}
+				&:after {
+					content: '';
+					position: absolute;
+					top: 0;
+					left: 0;
+					right: 0;
+					bottom: 0;
+					border-radius: 4px;
+				}
+			}
+		}
+		p {
+			position: relative;
+			margin: 0;
+			color: var(--light-gray);
+			font-size: functions.toRem(14);
 		}
 		img {
 			width: 100%;
