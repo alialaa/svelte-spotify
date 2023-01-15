c9ba0033182cd58d3e15dba764ca11d55d935fd1
Ali Alaa <hello@alialaa.com>
2023-01-15 12:14:19 +0200

    Code for home-data

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,8 +1,42 @@
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ fetch }) => {
-	const res = await fetch('/api/spotify/browse/new-releases?limit=6');
-	const res2 = await fetch('/api/spotify/browse/featured-playlists?limit=6');
-	console.log(await res.json());
-	console.log(await res2.json());
+export const load: PageLoad = async ({ fetch, parent }) => {
+	const { user } = await parent();
+	const newReleases = fetch('/api/spotify/browse/new-releases?limit=6');
+	const featuredPlaylists = fetch('/api/spotify/browse/featured-playlists?limit=6');
+	const userPlaylists = fetch(`/api/spotify/users/${user?.id}/playlists?limit=6`);
+
+	const catsRes = await fetch(`api/spotify/browse/categories`);
+	const catsResJSON: SpotifyApi.MultipleCategoriesResponse | undefined = catsRes.ok
+		? await catsRes.json()
+		: undefined;
+
+	const randomCats = catsResJSON
+		? catsResJSON.categories.items.sort(() => 0.5 - Math.random()).slice(0, 3)
+		: [];
+
+	const randomCatsPromises = randomCats.map((cat) =>
+		fetch(`/api/spotify/browse/categories/${cat.id}/playlists?limit=6`)
+	);
+
+	const [newReleasesRes, featuredPlaylistsRes, userPlaylistsRes, ...randomCatsRes] =
+		await Promise.all([newReleases, featuredPlaylists, userPlaylists, ...randomCatsPromises]);
+
+	return {
+		newReleases: newReleasesRes.ok
+			? (newReleasesRes.json() as Promise<SpotifyApi.ListOfNewReleasesResponse>)
+			: undefined,
+		featuredPlaylists: featuredPlaylistsRes.ok
+			? (featuredPlaylistsRes.json() as Promise<SpotifyApi.ListOfFeaturedPlaylistsResponse>)
+			: undefined,
+		userPlaylists: userPlaylistsRes.ok
+			? (userPlaylistsRes.json() as Promise<SpotifyApi.ListOfUsersPlaylistsResponse>)
+			: undefined,
+		homeCategories: randomCats,
+		categoriesPlaylists: Promise.all(
+			randomCatsRes.map((res) =>
+				res.ok ? (res.json() as Promise<SpotifyApi.CategoryPlaylistsResponse>) : undefined
+			)
+		)
+	};
 };
