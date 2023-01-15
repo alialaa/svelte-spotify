66ac9cc10b34206e2ffffe6ec63a14fe6baeddfb
Ali Alaa <hello@alialaa.com>
2023-01-15 14:09:22 +0200

    Code for home-markup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,70 @@
 <script lang="ts">
+	import { Button } from '$components';
+	import { element } from 'svelte/internal';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
+
+	let sections: {
+		title: string;
+		path: string;
+		items: (SpotifyApi.AlbumObjectSimplified | SpotifyApi.PlaylistObjectSimplified)[];
+	}[] = [];
+
+	$: {
+		if (data.newReleases) {
+			sections.push({
+				title: 'New Releases',
+				path: '/sections/new-releases',
+				items: data.newReleases.albums.items
+			});
+		}
+		if (data.featuredPlaylists) {
+			sections.push({
+				title: 'Featured Playlists',
+				path: '/sections/featured-playlists',
+				items: data.featuredPlaylists.playlists.items
+			});
+		}
+		data.homeCategories.forEach((category, index) => {
+			const categoryPlaylist = data.categoriesPlaylists[index];
+			if (categoryPlaylist) {
+				sections.push({
+					title: category.name,
+					path: `/category/${category.id}`,
+					items: categoryPlaylist.playlists.items
+				});
+			}
+		});
+		if (data.userPlaylists) {
+			sections.push({
+				title: 'Your Playlists',
+				path: '/playlists',
+				items: data.userPlaylists.items
+			});
+		}
+	}
 </script>
+
+{#each sections as section}
+	<section class="content-row">
+		<div class="content-row-header">
+			<div class="right">
+				<h2 class="section-title">{section.title}</h2>
+			</div>
+			<div class="left">
+				<Button element="a" href={section.path} variant="outline"
+					>See All <span class="visually-hidden">{section.title}</span></Button
+				>
+			</div>
+		</div>
+		<div class="grid-items">
+			{#each section.items as item}
+				<div class="grid-item" style="background-color: black;">
+					<h5>{item.type}</h5>
+					<h3>{item.name}</h3>
+				</div>
+			{/each}
+		</div>
+	</section>
+{/each}
