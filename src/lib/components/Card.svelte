abbb00926115645db2c74edb99e27ae324ae8e91
Ali Alaa <hello@alialaa.com>
2023-02-13 17:42:00 +0200

    Code for profile-page-2

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -6,6 +6,8 @@
 	type ArtistType = SpotifyApi.ArtistObjectFull;
 
 	export let item: AlbumType | PlaylistType | ArtistType;
+
+	const followersFormat = Intl.NumberFormat('en', { notation: 'compact' });
 </script>
 
 <div class="card {item.type}">
@@ -23,6 +25,9 @@
 	{#if item.type === 'playlist'}
 		<p class="truncate-2">{item.description}</p>
 	{/if}
+	{#if item.type === 'artist'}
+		<p class="truncate-1">{followersFormat.format(item.followers.total)} Followers</p>
+	{/if}
 </div>
 
 <style lang="scss">
@@ -33,6 +38,26 @@
 		border-radius: 4px;
 		transition: background 0.3s;
 		position: relative;
+		&.artist {
+			text-align: center;
+			padding: 30px 20px;
+			img,
+			.cover-placeholder {
+				width: 150px;
+				max-width: 100%;
+				border-radius: 100%;
+				box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
+			}
+			h4 {
+				margin: 0;
+				text-align: center;
+				font-size: functions.toRem(18);
+				margin-bottom: 0.5em;
+			}
+			p {
+				margin: 0;
+			}
+		}
 		&:hover {
 			background-color: var(--medium-gray);
 			.cover-placeholder {
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import { invalidate } from '$app/navigation';
+	import { Button } from '$components';
+	import Card from '$components/Card.svelte';
 	import ItemPage from '$components/ItemPage.svelte';
 	import type { PageData } from './$types';
 
@@ -6,6 +9,8 @@
 	$: user = data.user;
 	$: color = data.color;
 	$: following = data.following;
+
+	let isRetrying = false;
 
 	const followersFormat = Intl.NumberFormat('en', { notation: 'compact' });
 </script>
@@ -20,6 +25,39 @@
 		<span slot="meta" class="following-count"
 			>{followersFormat.format(user?.followers?.total || 0)} Followers</span
 		>
+
+		{#if following === undefined}
+			<div class="fail">
+				<p>Failed to load data.</p>
+				<Button
+					disabled={isRetrying}
+					element="button"
+					on:click={async () => {
+						isRetrying = true;
+						await invalidate((url) => url.pathname === '/api/spotify/me/following');
+						isRetrying = false;
+					}}>Retry</Button
+				>
+			</div>
+		{/if}
+
+		{#if following && following.artists.items.length > 0}
+			<div class="following">
+				<h2>Following</h2>
+				<div class="grid-items">
+					{#each following.artists.items as artist}
+						<div class="grid-item">
+							<Card item={artist} />
+						</div>
+					{/each}
+				</div>
+				<div class="view-all-button">
+					<Button element="a" variant="outline" href="/profile/artists"
+						>View All <span class="visually-hidden">artists that you are following</span></Button
+					>
+				</div>
+			</div>
+		{/if}
 	</ItemPage>
 </div>
 
@@ -38,6 +76,21 @@
 		.following-count {
 			font-size: functions.toRem(14);
 			color: var(--light-gray);
+		}
+		.fail {
+			text-align: center;
+			margin-top: 40px;
+		}
+		.following {
+			margin-top: 30px;
+			h2 {
+				font-size: functions.toRem(24);
+				margin-bottom: 0.7em;
+			}
+			.view-all-button {
+				margin-top: 30px;
+				text-align: right;
+			}
 		}
 	}
 </style>
